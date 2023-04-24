a4cd714afa0288b1a0fee612b03621f8edde3582
Rakesh Dasi <rakesh.d@browserstack.com>
2023-04-24 19:34:43 +0530

    Code for App-Automate

---
+++ src/test/java/com/setup/TestApp.java
@@ -0,0 +1,44 @@
+package com.setup;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import java.net.URL;
+public class TestApp {
+    public static AndroidDriver<MobileElement> driver;
+    static String userName = System.getenv("BROWSERSTACK_USERNAME");
+    static String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+    static String browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
+    static String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
+    static String browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
+    static String app = System.getenv("BROWSERSTACK_APP_ID");
+    private static final String BROWSERSTACK_HUB_URL = "hub-cloud.browserstack.com";
+
+    @Test
+    public void test_setup() throws Exception {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("app", app);
+        caps.setCapability("device", "Samsung Galaxy S8");
+        caps.setCapability("build", buildName);
+        caps.setCapability("browserstack.local", browserstackLocal);
+        caps.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
+        driver = new AndroidDriver<MobileElement>(new URL("https://" + userName + ":" + accessKey + "@" + BROWSERSTACK_HUB_URL + "/wd/hub"),caps);
+        Thread.sleep(3000);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(ITestResult result) throws Exception {
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        if (result.getStatus() == ITestResult.SUCCESS) {
+            jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Test Passed\"}}");
+        }
+        else
+            jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"failed\", \"reason\": \"Test Failed\"}}");
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
