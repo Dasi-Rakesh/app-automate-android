2c5c920126c82098c1e8cac4b153e32ea6ca09c9
Rakesh Dasi <rakesh.d@browserstack.com>
2023-04-25 00:40:30 +0530

    Test App Update

--- src/test/java/com/setup/TestApp.java
+++ src/test/java/com/setup/TestApp.java
@@ -24,21 +24,12 @@ public class TestApp {
 
     @Test
     public void test_setup() throws Exception {
-        bsLocal = new Local();
-        HashMap<String, String> bsLocalArgs = new HashMap<String, String>();
-        bsLocalArgs.put("key", accessKey);
-        bsLocal.start(bsLocalArgs);
-
-        while(!bsLocal.isRunning()){
-            Thread.sleep(1000);
-        }
-        System.out.println(bsLocal.isRunning());
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("app", app);
         caps.setCapability("device", "Samsung Galaxy S8");
         caps.setCapability("build", buildName);
         caps.setCapability("browserstack.local", browserstackLocal);
-       // caps.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
+        caps.setCapability("browserstack.localIdentifier", browserstackLocalIdentifier);
         driver = new AndroidDriver<MobileElement>(new URL("https://" + userName + ":" + accessKey + "@" + BROWSERSTACK_HUB_URL + "/wd/hub"),caps);
         Thread.sleep(3000);
     }
@@ -52,7 +43,6 @@ public class TestApp {
         else
             jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"failed\", \"reason\": \"Test Failed\"}}");
         Thread.sleep(3000);
-        bsLocal.stop();
         driver.quit();
     }
 }
